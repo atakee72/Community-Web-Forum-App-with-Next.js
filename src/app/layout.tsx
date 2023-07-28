602c2ce390ecdad71549bd30dec0079e07d5a745
atakee72 <atakee@gmail.com>
2023-07-28 02:05:54 +0200

    applied the first component - Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Community Web Forum",
@@ -12,11 +13,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Navbar />
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/app/user-profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserProfile() {
+  return <div>User profile</div>;
+}
+
+export default UserProfile;
