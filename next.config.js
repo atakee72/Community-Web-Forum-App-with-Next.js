2a5c446625b58e5942239452b7512e27d0c14b2c
atakee72 <atakee@gmail.com>
2023-10-16 21:23:58 +0200

    cleaned: unused config

--- next.config.js
+++ next.config.js
@@ -12,14 +12,6 @@ const nextConfig = {
   // },
   images: {
     domains: ["lh3.googleusercontent.com"],
-    // remotePatterns: [
-    //   {
-    //     // protocol: "https",
-    //     hostname: "",
-    //     // port: "",
-    //     // pathname: "/my-bucket/**",
-    //   },
-    // ],
   },
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
