574c27d7ce5c4f27b25651fd08f2d9ba11158e5b
atakee72 <atakee@gmail.com>
2024-03-09 23:05:59 +0100

    changed: the topic model so as to make it more concise

--- src/models/topicModel.ts
+++ src/models/topicModel.ts
@@ -2,11 +2,11 @@ import mongoose, { Schema } from "mongoose";
 
 const topicSchema = new Schema(
   {
-    title: String,
-    body: String,
+    title: { type: String, required: true }, // Title should be required
+    body: { type: String, required: true }, // Body should be required
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
-    views: Number,
-    likes: Number,
+    views: { type: Number, default: 0 }, // Default views to 0
+    likes: { type: Number, default: 0 }, // Default likes to 0
     likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
     tags: [{ type: String }],
   },
@@ -18,3 +18,26 @@ const topicSchema = new Schema(
 const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
 
 export default Topic;
+
+
+
+// import mongoose, { Schema } from "mongoose";
+
+// const topicSchema = new Schema(
+//   {
+//     title: String,
+//     body: String,
+//     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comment" }],
+//     views: Number,
+//     likes: Number,
+//     likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+//     tags: [{ type: String }],
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const Topic = mongoose.models.Topic || mongoose.model("Topic", topicSchema);
+
+// export default Topic;
